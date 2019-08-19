8e497640fb68ab610321dd1947f4cdb09cd272eb
Cheryl Chua <cherylsychua@outlook.com>
2019-08-19 14:42:42 +0100

    Add husky pre commit hook to run tests and prettier

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const expressValidation = require('express-validation');
-const Joi = require('@hapi/joi');
 const bodyParser = require('body-parser');
-const knex = require('knex')(require('../knexfile').development)
+const knex = require('knex')(require('../knexfile').development);
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,17 +8,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 3000;
 
-app.get(
-    '/organisation-units',
-    async function(req, res, next) {
-        try {
-            const organisationUnits = await knex('organisationunits').select('*')
-            return res.json(organisationUnits);
-        } catch (err) {
-            next(err);
-        }
+app.get('/organisation-units', async function(req, res, next) {
+    try {
+        const organisationUnits = await knex('organisationunits').select('*');
+        return res.json(organisationUnits);
+    } catch (err) {
+        next(err);
     }
-);
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`);
